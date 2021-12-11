ea2de88d0773a3cde5aa4f4bfe34b0a3e130ce4f
Khandaker Rifah Tasnia <rifahtasnia@iut-dhaka.edu>
2021-12-11 19:02:02 +0600

    working on carousel

--- src/carousel.js
+++ src/carousel.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Carousel from 'react-material-ui-carousel'
-import { Paper, Button } from '@mui/material'
-import MediaControlCard from "./views/SmallComponent";
+import Container from '@mui/material/Container';
 
 import { useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
@@ -14,40 +13,49 @@ import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import SkipNextIcon from '@mui/icons-material/SkipNext';
 
-const CarouselTest = () => 
-{
-    var items = [
-        {
-            name: "Random Name #1",
-            description: "Probably the most random thing you have ever seen!"
-        },
-        {
-            name: "Random Name #2",
-            description: "Hello World!"
-        }
-    ]
+import myimg from "./assets/images/PS farewell 1.JPG";
+import myimg2 from "./assets/images/PS farewell 2.JPG";
 
-    return (
-        <Carousel fullHeightHover={false}     // We want the nav buttons wrapper to only be as big as the button element is
-        navButtonsProps={{          // Change the colors and radius of the actual buttons. THIS STYLES BOTH BUTTONS
-            style: {
-                backgroundColor: 'cornflowerblue',
-                borderRadius: 0
-            }
-        }} 
-        navButtonsWrapperProps={{   // Move the buttons to the bottom. Unsetting top here to override default style.
-            style: {
-                bottom: '0',
-                top: 'unset'
-            }
-        }} 
-        NextIcon='next'             // Change the "inside" of the next button to "next"
-        PrevIcon='prev'   >
-            {
-                items.map( (item, i) => <Item key={i} item={item} /> )
-            }
-        </Carousel>
-    )
+const CarouselTest = () => {
+  var items = [
+    {
+      name: "Random Name #1",
+      description: "Probably the most random thing you have ever seen!",
+      image: myimg
+    },
+    {
+      name: "Random Name #2",
+      description: "Hello World!",
+      image: myimg2
+    },
+    {
+      name: "Random Name #2",
+      description: "Hello World!",
+      image: myimg
+    }
+  ]
+
+  return (
+    <Carousel fullHeightHover={false}     // We want the nav buttons wrapper to only be as big as the button element is
+      navButtonsProps={{          // Change the colors and radius of the actual buttons. THIS STYLES BOTH BUTTONS
+        style: {
+          backgroundColor: 'cornflowerblue',
+          borderRadius: 0
+        }
+      }}
+      navButtonsWrapperProps={{   // Move the buttons to the bottom. Unsetting top here to override default style.
+        style: {
+          bottom: '0',
+          top: 'unset'
+        }
+      }}
+      NextIcon='next'             // Change the "inside" of the next button to "next"
+      PrevIcon='prev'   >
+      {
+        items.map((item, i) => <Item key={i} item={item} />)
+      }
+    </Carousel>
+  )
 }
 
 
@@ -55,35 +63,16 @@ function Item(props) {
   const theme = useTheme();
 
   return (
-    <Card sx={{ display: 'flex' }}>
-      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-        <CardContent sx={{ flex: '1 0 auto' }}>
-          <Typography component="div" variant="h5">
-          {props.item.name}
-          </Typography>
-          <Typography variant="subtitle1" color="text.secondary" component="div">
-          {props.item.name}
-          </Typography>
-        </CardContent>
-        <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
-          <IconButton aria-label="previous">
-            {theme.direction === 'rtl' ? <SkipNextIcon /> : <SkipPreviousIcon />}
-          </IconButton>
-          <IconButton aria-label="play/pause">
-            <PlayArrowIcon sx={{ height: 38, width: 38 }} />
-          </IconButton>
-          <IconButton aria-label="next">
-            {theme.direction === 'rtl' ? <SkipPreviousIcon /> : <SkipNextIcon />}
-          </IconButton>
-        </Box>
-      </Box>
-      <CardMedia
-        component="img"
-        sx={{ width: 151 }}
-        image="/static/images/cards/live-from-space.jpg"
-        alt="Live from space album cover"
-      />
-    </Card>
+    <Container maxWidth="xl">
+      <Card sx={{ display: 'flex' }}>
+        <CardMedia
+          component="img"
+          sx={{ height: 700 }}
+          image={props.item.image}
+          alt="Live from space album cover"
+        />
+      </Card>
+    </Container>
   );
 }
 
