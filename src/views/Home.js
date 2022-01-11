57243d653d444231e9dbfc38f3441e6dd680a139
Zubair <zubairtusar17@gmail.com>
2022-01-11 22:21:17 +0600

    fix: carousel in home page

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,29 +1,24 @@
 import React, { useState } from "react";
 import SmallComponent from "./SmallComponent";
 import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
-import ParallaxScroll from '../utils/ParallaxScroll';
-import CarouselTest from './../carousel';
-
+import ParallaxScroll from "../utils/ParallaxScroll";
+import CarouselTest from "./../carousel";
 
 const Home = () => {
   const [toggleDark, settoggleDark] = useState(false);
   const myTheme = createMuiTheme({
-    
     // Theme settings
     palette: {
-      type: "dark"
+      type: "dark",
     },
   });
-  
+
   return (
-    
     // Wrapping code in ThemeProvider
     <ThemeProvider theme={myTheme}>
       <ParallaxScroll />
-      
-      <CarouselTest />
     </ThemeProvider>
   );
-}
+};
 
 export default Home;
