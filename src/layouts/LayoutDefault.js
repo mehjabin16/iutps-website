c75b07049ea8f179d9d4501f11b2477f28c3a9e9
Zubair <zubairtusar17@gmail.com>
2022-01-11 22:17:44 +0600

    feat: header, footer, logo updated

--- src/layouts/LayoutDefault.js
+++ src/layouts/LayoutDefault.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import Header from '../components/layout/Header';
-import Footer from '../components/layout/Footer';
+import React from "react";
+import Header from "../components/layout/Header";
+import Footer from "../components/layout/Footer";
 
 const LayoutDefault = ({ children }) => (
   <>
-    
     <Header />
-    <div className="site-content">
-      {children}
-    </div>
+    <div className="site-content">{children}</div>
     <Footer />
-    
   </>
 );
 
-export default LayoutDefault;  
+export default LayoutDefault;
