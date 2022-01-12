e3b49ef218943c5ce04221fd4a19b094a00398ae
Zubair <zubairtusar17@gmail.com>
2022-01-12 09:26:50 +0600

    feat: basic gallery page. run npm i

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import LayoutDefault from "./layouts/LayoutDefault";
 import Home from "./views/Home";
 import Test from "./views/Test";
 import CarouselTest from "./carousel";
-import Gallery from "./views/Gallery";
+import GalleryView from "./views/GalleryView";
 
 // Initialize Google Analytics
 ReactGA.initialize(process.env.REACT_APP_GA_CODE);
@@ -41,7 +41,7 @@ const App = () => {
       <AppRoute
         exact
         path="/gallery"
-        component={Gallery}
+        component={GalleryView}
         layout={LayoutDefault}
       />
     </Switch>
