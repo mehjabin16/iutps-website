7a07c1515aa93d0a66df9ec840583b53b2c576d1
Khandaker Rifah Tasnia <rifahtasnia@iut-dhaka.edu>
2022-01-19 21:22:22 +0600

    Update carousel.js

--- src/carousel.js
+++ src/carousel.js
@@ -3,31 +3,39 @@ import Carousel from 'react-material-ui-carousel'
 import Container from '@mui/material/Container';
 
 import { useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import SkipNextIcon from '@mui/icons-material/SkipNext';
 
 import CircleIcon from '@mui/icons-material/Circle';
 
 
 import myimg from "./assets/images/PS farewell 1.JPG";
 import myimg2 from "./assets/images/PS farewell 2.JPG";
+import myimg3 from "./assets/images/Photo1.jpg";
 
 const CarouselTest = () => {
   var items = [
     {
       name: "Random Name #1",
-      description: "Probably the most random thing you have ever seen!",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
       image: myimg
     },
     {
       name: "Random Name #2",
-      description: "Hello World!",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
       image: myimg2
     },
     {
       name: "Random Name #2",
-      description: "Hello World!",
-      image: myimg
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      image: myimg3
     }
   ]
 
@@ -69,6 +77,17 @@ function Item(props) {
           image={props.item.image}
           alt="Live from space album cover"
         />
+        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+        <CardContent sx={{ flex: '1 0 auto' }}>
+            <Typography component="div" variant="h5">
+              {props.item.name}
+            </Typography>
+
+            <Typography variant="caption" display="block" gutterBottom>
+            {props.item.description}
+      </Typography>
+        </CardContent>
+      </Box>
       </Card>
     </Container>
   );
